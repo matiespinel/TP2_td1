3480b3512c0ae89e6d98a7d3206fdc25210907d8
matiespinel <matiasespinel.me@gmail.com>
2025-06-11 12:21:41 -0300

    Update pais.py

--- pais.py
+++ pais.py
@@ -1,17 +1,16 @@
 from resumen import Resumen
 from estudiante import Estudiante
+import csv
 
 class Pais:
     def __init__(self, archivo_csv:str):
-        ''' completar docstring '''
-        pass
-          """ Inicializa un objeto Pais a partir de un archivo CSV con datos de estudiantes.
+        """ Inicializa un objeto Pais a partir de un archivo CSV con datos de estudiantes.
               Parámetro:
              - archivo_csv con las columnas:
              'provincia', 'puntaje_matematica', 'puntaje_lengua',
              'puntaje_nse', 'ambito', 'sector'
-          """
-        self.estudiantes: List[Estudiante] = []
+        """
+        self.estudiantes: list[Estudiante] = []
 
         f = open(archivo_csv)
         for linea in csv.DictReader(f):
@@ -24,48 +23,38 @@ class Pais:
 
             est: Estudiante = Estudiante(prov, mat, leng, nse, amb, sec)
             self.estudiantes.append(est)
+        f.close()
+        self.provincias: set[str] = set(e.provincia for e in self.estudiantes)
 
-        self.provincias: Set[str] = set(e.provincia for e in self.estudiantes)
-
-    def tamano(self, ...) -> ...:
-        ''' completar docstring '''
-        pass
     def tamano(self) -> int:
         """ Devuelve la cantidad de estudiantes en el dataset.
             Complejidad: O(1).
         """
         return len(self.estudiantes)
 
-    def resumen_provincia(self, ...) -> ...:
-        ''' completar docstring '''
-        pass
+
     def resumen_provincia(self, provincia:str) -> Resumen:
-         """ Devuelve un resumen con estadísticas de estudiantes de una provincia. 
+        """ Devuelve un resumen con estadísticas de estudiantes de una provincia. 
              Parámetros:
              Provincia: código de la provincia (ej: 'BA', 'MZA', 'SFE').
              Complejidad: O(N), siendo N la cantidad de estudiantes.
         """
-        estudiantes: List[Estudiante] = [e for e in self.estudiantes if e.provincia == provincia]
+        estudiantes: list[Estudiante] = [e for e in self.estudiantes if e.provincia == provincia]
         return Resumen(estudiantes)
 
-    def resumenes_pais(self) -> ...:
-        ''' completar docstring '''
-        pass
-    def resumenes_pais(self) -> Dict[str, Resumen]:
+
+    def resumenes_pais(self) -> dict[str, Resumen]:
         """ Devuelve un diccionario con un resumen por cada provincia presente en el dataset.
             Claves: códigos de provincia.
             Valores: objetos de la clase Resumen.
             Complejidad: O(N*P), donde N es la cantidad de estudiantes y P la cantidad de provincias.
         """
-        res: Dict[str, Resumen] = {}
+        res: dict[str, Resumen] = {}
         for prov in self.provincias:
             res[prov] = self.resumen_provincia(prov)
         return res
 
-    def estudiantes_en_intervalo(self, ...) -> ...:
-        ''' completar docstring '''
-        pass
-    def estudiantes_en_intervalo(self, categoria: str, x: float, y: float, provincias: Set[str]) -> int:
+    def estudiantes_en_intervalo(self, categoria: str, x: float, y: float, provincias: set[str]) -> int:
         """ Devuelve la cantidad de estudiantes en el intervalo [x, y) para cierta categoría,
             dentro de las provincias indicadas.
             Parámetros:
@@ -75,42 +64,37 @@ class Pais:
             Complejidad: O(N*P), donde N es la cantidad total de estudiantes.
         """
         contador: int = 0
-        for e in self._estudiantes:
+        for e in self.estudiantes:
             if e.provincia in provincias:
                 valor: float = 0.0
                 if categoria == "mat":
                     valor = e.puntaje_matematica
                 elif categoria == "len":
                     valor = e.puntaje_lengua
-                else categoria == "nse":
+                elif categoria == "nse":
                     valor = e.puntaje_nse
                 if x <= valor < y:
                     contador = contador + 1
         return contador
 
-    def exportar_por_provincias(self, ...):
-        ''' completar docstring '''
-        pass
-
-    def exportar_por_provincias(self, archivo_csv: str, provincias: Set[str]) -> None:
+    def exportar_por_provincias(self, archivo_csv: str, provincias: set[str]) -> None:
         """Genera un archivo CSV con un resumen por cada provincia del conjunto dado.
            Columnas:
           provincia, cantidad, promedio_matematica, promedio_lengua,
           promedio_nse, proporcion_ambito_rural, proporcion_sector_estatal
         Complejidad: O(N*P).
         """
-    with open(archivo_csv, "w") as archivo:
-    encabezado = "provincia,cantidad,promedio_matematica,promedio_lengua,promedio_nse,proporcion_ambito_rural,proporcion_sector_estatal\n"
-    archivo.write(encabezado)
-    for prov in provincias:
-        resumen = self.resumen_provincia(prov)        
-        linea = (
-            prov + "," +
-            str(round(resumen.cantidad, 2)) + "," +
-            str(round(resumen.promedio_matematica, 2)) + "," +
-            str(round(resumen.promedio_lengua, 2)) + "," +
-            str(round(resumen.promedio_nse, 2)) + "," +
-            str(round(resumen.proporcion_ambito_rural, 2)) + "," +
-            str(round(resumen.proporcion_sector_estatal, 2)) + "\n"
-        )
-        archivo.write(linea)
+        with open(archivo_csv, "w") as archivo:
+            encabezado = "provincia,cantidad,promedio_matematica,promedio_lengua,promedio_nse,proporcion_ambito_rural,proporcion_sector_estatal\n"
+            archivo.write(encabezado)
+            for prov in provincias:
+                resumen = self.resumen_provincia(prov)        
+                linea = (
+                    prov + "," +
+                    str(round(resumen.cantidad, 2)) + "," +
+                    str(round(resumen.promedio_matematica, 2)) + "," +
+                    str(round(resumen.promedio_lengua, 2)) + "," +
+                    str(round(resumen.promedio_NSE, 2)) + "," +
+                    str(round(resumen.proporcion_ambito_rural, 2)) + "," +
+                    str(round(resumen.proporcion_sector_estatal, 2)) + "\n")
+            archivo.write(linea)
